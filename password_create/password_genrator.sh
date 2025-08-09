ab3728824d42f756115080d3db9e7d1e840a139d
sonuJha108 <codinghub650@gmail.com>
2025-08-09 16:28:17 +0530

    upload the projects folder

---
+++ password_create/password_genrator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Welcome to simple password generator"
+
+sleep 3
+
+echo "Please enter the length of the password"
+read PASS_LENGTH
+
+for p in $(seq 1);
+do
+	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+done
+
+
